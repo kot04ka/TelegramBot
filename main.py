6cabcf29bdbc7c6cd044e516b5c0634d46d13e02
kot04ka <kostakovalenko7@gmail.com>
2024-02-27 14:04:44 +0200

    Fix main.py
    Изменена структура кода исправлено читаемость

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
+# Импорт необходимых библиотек
 import telebot
 import mysql.connector
 from threading import Timer
 import threading
 import schedule
 import time
-
+from telebot import types
 from datetime import datetime, timedelta
 from dateutil.parser import parse
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -13,8 +13,9 @@ import random
 from advice_lists import advice_start, advice_action, advice_end
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-#Token бота 
+# Токен бота 
 bot = telebot.TeleBot('6166430773:AAHqpvwfB2eY7nJXUa4EdZ8kNDkr9zFrP8I')
+
 
 # Подключение к базе данных MySQL
 db = mysql.connector.connect(
@@ -25,44 +26,85 @@ db = mysql.connector.connect(
 )
 cursor = db.cursor()
 
+
+#================================Базовые Функции========================================
+#================================Базовые Функции========================================
+
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     user_id = message.from_user.id
+    # Запрос к базе данных для получения имени пользователя
     cursor.execute("SELECT fullname FROM users WHERE user_id = %s", (user_id,))
     user = cursor.fetchone()
+    # Если пользователь найден в базе данных, приветствуем его по имени
     if user is not None:
         bot.reply_to(message, f"Привет, {user[0]}! Чем я могу помочь вам сегодня?")
+    # Если пользователь не найден в базе данных, приветствуем его и просим ввести ФИО
     else:
         bot.reply_to(message, "Привет! Я ваш виртуальный психолог. Чем я могу помочь вам сегодня?")
         bot.send_message(message.chat.id, "Пожалуйста, введите ваше ФИО:")
         bot.register_next_step_handler(message, save_fullname)
+
+# Функция для сохранения ФИО пользователя в базе данных
 def save_fullname(message):
     user_id = message.from_user.id
     fullname = message.text
+    # Запрос на вставку данных пользователя в базу данных
     sql = "INSERT INTO users (user_id, fullname) VALUES (%s, %s)"
     val = (user_id, fullname)
     cursor.execute(sql, val)
     db.commit()
+    # После сохранения ФИО пользователя, просим его ввести номер телефона
     bot.send_message(message.chat.id, f"Спасибо, {fullname}! Теперь введите ваш номер телефона или напишите 'нет', если не хотите указывать:")
     bot.register_next_step_handler(message, save_phone)
 
+# Функция для сохранения номера телефона пользователя в базе данных
 def save_phone(message):
     user_id = message.from_user.id
     phone = message.text
+    # Если пользователь не хочет указывать номер телефона, сохраняем вместо него '#'
     if phone.lower() == 'нет':
         phone = '#'
+    # Запрос на обновление номера телефона пользователя в базе данных
     sql = "UPDATE users SET phone = %s WHERE user_id = %s"
     val = (phone, user_id)
     cursor.execute(sql, val)
     db.commit()
+    # После сохранения номера телефона пользователя, благодарим его
     bot.send_message(message.chat.id, "Спасибо! Ваш номер телефона сохранен." if phone != '#' else "Спасибо! Выбор без указания номера телефона сохранен.")
 
-#Обработка команды тесты
+# Обработчик для других сообщений
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    # Если бот не понял сообщение пользователя, он просит пользователя ввести команду /help
+    bot.reply_to(message, "Извините, я не понял вашего сообщения. Если вам нужна помощь, введите /help.")
+
+# Обработчик команды для отображения информации о помощи
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    # Отправляем пользователю информацию о доступных командах
+    help_text = """
+    Добро пожаловать
+    Вы можете использовать следующие команды:
+    /start - Начать взаимодействие с психологом
+    /tests - Посмотреть все тесты :
+    """
+    bot.reply_to(message, help_text)
+
+
+#================================ Конец Базовым Функциям ========================================
+#================================ Конец Базовым Функциям ========================================
+
+
+
+
+#================================ Тестирование ========================================
+#================================ Тестирование ========================================
+
+# Обработчик команды для начала тестирования
 def start_test(message):
     bot.reply_to(message,"Выберете тест который вы хотите пройти ")
-
-
-#test
 
 # Обработчик команды для прохождения теста на уровень тревожности
 @bot.message_handler(commands=['test-1'])
@@ -73,16 +115,22 @@ def start_anxiety_test(message):
     bot.send_message(message.chat.id, "1. Чувствуете ли вы постоянное беспокойство и нервозность?")
     bot.register_next_step_handler(message, question_2)
 
+# Функция для обработки второго вопроса теста
 def question_2(message):
     try:
         response = int(message.text)
+        # Если ответ не в диапазоне от 1 до 5, вызываем исключение
         if response < 1 or response > 5:
             raise ValueError
-
+        
+        # Проверяем, есть ли уже словарь для этого пользователя
+        if message.from_user.id not in user_data:
+            user_data[message.from_user.id] = {}
+            
         # Сохраняем ответ пользователя
         user_data[message.from_user.id]['Вопрос 1'] = response
 
-        # Connect to the database
+        # Подключаемся к базе данных
         db = mysql.connector.connect(
             host="localhost",
             user="kot04ka",
@@ -91,7 +139,7 @@ def question_2(message):
         )
         cursor = db.cursor()
 
-        # Get the user's fullname and phone_number from the database
+        # Получаем ФИО и номер телефона пользователя из базы данных
         user_id = message.from_user.id
         cursor.execute("SELECT fullname, phone FROM users WHERE user_id = %s", (user_id,))
         user = cursor.fetchone()
@@ -103,18 +151,20 @@ def question_2(message):
         # Преобразуем словарь с ответами пользователя в строку
         answers = str(user_data[message.from_user.id])
 
+
         # Получаем текущую дату и время
         test_date = datetime.now()
 
-        # Insert the data into the testresults table
+        # Вставляем данные в таблицу testresults
         query = "INSERT INTO testresults (fullname, phone_number, answers, test_date) VALUES (%s, %s, %s, %s)"
         values = (fullname, phone_number, answers, test_date)
 
         cursor.execute(query, values)
 
-        # Commit the transaction
+        # Подтверждаем транзакцию
         db.commit()
 
+        # Если уровень тревожности высокий, рекомендуем обратиться к специалисту
         if response >= 4:
             bot.send_message(message.chat.id, "Ваш уровень тревожности высокий. Рекомендуется обратиться к специалисту.")
         else:
@@ -136,16 +186,18 @@ def start_yesno_test(message):
     bot.send_message(message.chat.id, "1. Вопрос 1?")
     bot.register_next_step_handler(message, yesno_question_2)
 
+# Функция для обработки второго вопроса теста
 def yesno_question_2(message):
     try:
         response = message.text.lower()
+        # Если ответ не 'да' или 'нет', вызываем исключение
         if response not in ['да', 'нет']:
             raise ValueError
 
         # Сохраняем ответ пользователя
         user_data[message.from_user.id]['Вопрос 1'] = response
 
-        # Continue with the next question
+        # Продолжаем с следующим вопросом
         bot.send_message(message.chat.id, "2. Вопрос 2?")
         bot.register_next_step_handler(message, yesno_question_3)
     except ValueError:
@@ -162,7 +214,7 @@ def yesno_question_3(message):
         # Сохраняем ответ пользователя
         user_data[message.from_user.id]['Вопрос 2'] = response
 
-        # Continue with the next question
+        # Продолжаем с следующим вопросом
         bot.send_message(message.chat.id, "3. Вопрос 3?")
         bot.register_next_step_handler(message, yesno_question_4)
     except ValueError:
@@ -178,7 +230,7 @@ def yesno_question_4(message):
         # Сохраняем ответ пользователя
         user_data[message.from_user.id]['Вопрос 3'] = response
 
-        # Connect to the database
+        # Подключаемся к базе данных
         db = mysql.connector.connect(
             host="localhost",
             user="kot04ka",
@@ -187,7 +239,8 @@ def yesno_question_4(message):
         )
         cursor = db.cursor()
 
-        # Get the user's fullname and phone_number from the database
+
+        # Получаем ФИО и номер телефона пользователя из базы данных
         user_id = message.from_user.id
         cursor.execute("SELECT fullname, phone FROM users WHERE user_id = %s", (user_id,))
         user = cursor.fetchone()
@@ -202,16 +255,16 @@ def yesno_question_4(message):
         # Получаем текущую дату и время
         test_date = datetime.now()
 
-        # Insert the data into the test2results table
+        # Вставляем данные в таблицу test2results
         query = "INSERT INTO test2results (fullname, phone_number, answers, test_date) VALUES (%s, %s, %s, %s)"
         values = (fullname, phone_number, answers, test_date)
 
         cursor.execute(query, values)
 
-        # Commit the transaction
+        # Подтверждаем транзакцию
         db.commit()
 
-        # Finish the test
+        # Завершаем тест
         bot.send_message(message.chat.id, "Спасибо за прохождение теста!")
     except ValueError:
         bot.send_message(message.chat.id, "Пожалуйста, ответьте 'да' или 'нет'.")
@@ -219,6 +272,12 @@ def yesno_question_4(message):
 
 
 
+#================================ Конец Тестирование ========================================
+#================================ Конец Тестирование ========================================
+
+
+#================================ Трекер напоминаний ========================================
+#================================ Трекер напоминаний ========================================
 # Трекер напоминаний
 reminders = {}
 
@@ -247,7 +306,7 @@ def set_reminder(chat_id, text, datetime_str, interval):
         return
 
     # Устанавливаем напоминание в планировщике заданий
-    if interval in [1, 2]:  # одноразовое напоминание
+    if interval in [1]:  # одноразовое напоминание
         scheduler.add_job(send_reminder, 'date', run_date=remind_datetime, args=[chat_id, text])
     elif interval > 2:  # повторяющееся напоминание
         scheduler.add_job(send_reminder, 'interval', days=interval, start_date=remind_datetime, args=[chat_id, text])
@@ -274,12 +333,15 @@ def handle_remind(message):
     set_reminder(chat_id, text, datetime_str, interval)
     bot.reply_to(message, 'Напоминание установлено!')
 
+#================================ Конец Трекер напоминаний ========================================
+#================================ Конец Трекер напоминаний ========================================
 
+#================================ Функция Советы по сну ========================================
+#================================ Функция Советы по сну ========================================
 
-
-
-#Советы по сну 
+# Функция для получения совета по сну
 def get_sleep_tip():
+    # Выбираем случайный совет из списка
     advice = random.choice(advice_start) + ' ' + random.choice(advice_action) + ' ' + random.choice(advice_end)
     return advice
 
@@ -287,51 +349,57 @@ def get_sleep_tip():
 @bot.message_handler(commands=['sleep_tip'])
 def handle_sleep_tip(message):
     chat_id = message.chat.id
+    # Получаем совет и отправляем его пользователю
     sleep_tip = get_sleep_tip()
     bot.send_message(chat_id, sleep_tip)
 
+#================================ Конец Функция Советы по сну ========================================
+#================================ Конец Функция Советы по сну ========================================
 
 
-#Кнопки
+#================================ Обработка всех кнопок ========================================
+#================================ Обработка всех кнопок ========================================
+# Обработчик команды /tests
+@bot.message_handler(commands=['tests'])
+def send_tests(message):
+    # Создаем кнопки для выбора теста
+    markup = types.InlineKeyboardMarkup()
+    test1_button = types.InlineKeyboardButton('Тест 1', callback_data='test-1')
+    test2_button = types.InlineKeyboardButton('Тест 2', callback_data='test-2')
+    markup.add(test1_button, test2_button)
+    # Отправляем сообщение с кнопками
+    bot.send_message(message.chat.id, "Выберите тест:", reply_markup=markup)
 
+# Обработчик нажатий на кнопки
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    # Запускаем соответствующий тест в зависимости от выбора пользователя
+    if call.data == 'test-1':
+        start_anxiety_test(call.message)
+    elif call.data == 'test-2':
+        start_yesno_test(call.message)
 
-
-
-
-
-
-
-
-
-
-
-# Обработчик для других сообщений
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "Извините, я не понял вашего сообщения. Если вам нужна помощь, введите /help.")
-
-
-# Обработчик команды для отображения информации о помощи
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    help_text = """
-    Добро пожаловать
-    Вы можете использовать следующие команды:
-    /start - Начать взаимодействие с психологом
-    /tests - Посмотреть все тесты :
-    /help - Получить информацию о командах бота
-    """
-    bot.reply_to(message, help_text)
+#================================ конец Обработoк всех кнопок ========================================
+#================================ конец Обработoк всех кнопок ===================================
 
 
 
+#================================ Функция для запуска бота ========================================
+#================================ Функция для запуска бота ========================================
+        
+        
+# Функция для запуска планировщика заданий
 def run_schedule():
     while True:
+        # Запускаем все запланированные задания
         schedule.run_pending()
         time.sleep(1)
 
 # Запуск цикла в отдельном потоке
 schedule_thread = threading.Thread(target=run_schedule)
 schedule_thread.start()
+# Запуск бота
 bot.polling()
+#================================ Функция для запуска бота ========================================
+#================================ Функция для запуска бота ========================================
 
